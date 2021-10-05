a0e4a4efe38477fc15396e074a683b5e32f324e4
Henrique Lopes <henriquelopees7@gmail.com>
2021-10-05 19:24:03 -0300

    Initial commit

---
+++ src/main/java/com/deveficiente/blinkistdojo/BlinkistDojoApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.blinkistdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkistDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkistDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/blinkistdojo/BlinkistDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.blinkistdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkistDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
